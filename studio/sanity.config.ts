35d20874da51026c8df73a20170e69f56256e45a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-12 18:13:27 +0200

    ➕ Studio: Add and configure Dashboard and Vercel Dashboard Widget

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,5 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {dashboardTool, projectInfoWidget} from '@sanity/dashboard'
+import {vercelWidget} from 'sanity-plugin-dashboard-widget-vercel'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
@@ -7,12 +9,15 @@ import {structure} from './structure'
 export default defineConfig({
   name: 'default',
   title: 'marcusbillman.com-2024',
-
   projectId: '2l3afo6f',
   dataset: 'production',
-
-  plugins: [structureTool({structure}), visionTool()],
-
+  plugins: [
+    structureTool({structure}),
+    dashboardTool({
+      widgets: [vercelWidget({layout: {width: 'medium'}}), projectInfoWidget()],
+    }),
+    visionTool(),
+  ],
   schema: {
     types: schemaTypes,
   },
