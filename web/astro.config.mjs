a83bf46c1d04a7fa725fac3cabb2eac4bd1f1daa
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-12 13:32:13 +0200

    ➕ Web: Add and configure Sanity Client

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: '2l3afo6f',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    }),
+  ],
 });
