6e12acfdf614cd11e2c60dede34a93bd6e4059b2
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-15 15:32:27 +0200

    ✨ Web: Add DotGrid component for background overlay

---
+++ web/src/components/DotGrid.tsx
@@ -0,0 +1,5 @@
+export default function DotGrid() {
+  return (
+    <div className="bg-overlay-dim-default absolute inset-0 -z-10 bg-[url('/assets/images/tiled/dot-light.svg')] bg-center dark:bg-[url('/assets/images/tiled/dot-dark.svg')]"></div>
+  );
+}
