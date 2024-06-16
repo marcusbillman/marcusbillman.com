81298f909a1acd92d0cecc726d7dcaff6d40e45b
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-16 22:27:49 +0200

    💄 Bio: Make heading h2 and increase font weight

--- web/src/components/Bio.tsx
+++ web/src/components/Bio.tsx
@@ -3,13 +3,13 @@ import Button from '@/components/Button';
 export default function Bio() {
   return (
     <section className="mt-[90vh] flex flex-col items-center gap-12 px-4 pb-24 pt-32 lg:mt-[100vh] lg:px-16 lg:pb-32 lg:pt-64">
-      <h1 className="text-balance text-center text-4xl lg:text-6xl">
+      <h2 className="text-balance text-center text-4xl font-medium lg:text-6xl">
         Hej!{' '}
         <span className="animate-wiggle inline-block origin-bottom-right">
           👋
         </span>{' '}
         I'm Marcus.
-      </h1>
+      </h2>
       <p className="max-w-[40ch] text-balance text-center text-2xl text-subtle lg:text-6xl">
         I love designing interfaces, writing code and spreading the joy of
         making cool things. I’m also a tech enthusiast and hobbyist musician 🎹.
