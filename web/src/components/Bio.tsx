db187006033d76a893afa24011ffa2146c3a5cf9
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-18 14:13:50 +0200

    💄 Bio: Make button size responsive

--- web/src/components/Bio.tsx
+++ web/src/components/Bio.tsx
@@ -5,7 +5,7 @@ export default function Bio() {
     <section className="mt-[90vh] flex flex-col items-center gap-12 px-4 pb-24 pt-32 lg:mt-[100vh] lg:px-16 lg:pb-32 lg:pt-64">
       <h2 className="text-balance text-center text-4xl font-medium lg:text-6xl">
         Hej!{' '}
-        <span className="animate-wiggle inline-block origin-bottom-right">
+        <span className="inline-block origin-bottom-right animate-wiggle">
           👋
         </span>{' '}
         I'm Marcus.
@@ -14,7 +14,12 @@ export default function Bio() {
         I love designing interfaces, writing code and spreading the joy of
         making cool things. I’m also a tech enthusiast and hobbyist musician 🎹.
       </p>
-      <Button text="About me" href="about" size="large" style="primary" />
+      <div className="lg:hidden">
+        <Button text="About me" href="about" size="medium" style="primary" />
+      </div>
+      <div className="hidden lg:block">
+        <Button text="About me" href="about" size="large" style="primary" />
+      </div>
     </section>
   );
 }
