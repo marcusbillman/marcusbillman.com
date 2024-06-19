adee4d517d7594ecf93eb0bc30ced0ca0383ea44
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-19 20:52:20 +0200

    💄 FeaturedWork: Improve responsiveness

--- web/src/components/sections/FeaturedWork.tsx
+++ web/src/components/sections/FeaturedWork.tsx
@@ -19,19 +19,19 @@ export default function FeaturedWork() {
 
 function FeaturedProject() {
   return (
-    <li className="flex flex-col-reverse gap-6 bg-default py-6 lg:flex-row lg:gap-0 lg:bg-transparent lg:py-0">
-      <div className="sticky top-16 flex h-fit flex-col gap-6 bg-default lg:w-full lg:gap-8 lg:py-16 lg:pr-16">
+    <li className="flex flex-col-reverse gap-6 bg-default py-6 lg:flex-row lg:items-center lg:gap-0 lg:bg-transparent lg:py-0 2xl:items-start">
+      <div className="top-16 flex h-fit flex-col gap-6 bg-default lg:w-full lg:gap-8 lg:py-8 lg:pr-16 2xl:sticky 2xl:py-16">
         <div className="flex flex-col gap-2 lg:gap-6">
           <h3 className="font-serif text-xl font-medium text-primary">
             Project name
           </h3>
-          <p className="text-balance text-3xl lg:text-6xl">
+          <p className="text-balance text-3xl lg:text-4xl 2xl:text-6xl">
             Project headline, lorem ipsum dolor sit amet
           </p>
         </div>
         <Button text="View case" href="#" />
       </div>
-      <div className='h-96 rounded-2xl bg-[url("https://picsum.photos/1000/1000")] bg-cover lg:aspect-square lg:h-auto lg:w-full'></div>
+      <div className='h-96 rounded-2xl bg-[url("https://picsum.photos/1000/1000")] bg-cover lg:h-[30vw] lg:w-full'></div>
     </li>
   );
 }
