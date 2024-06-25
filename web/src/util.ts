290640eec45985ca09a47faaa314371b79dd18ae
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-25 22:05:02 +0200

    ✨ util.ts: Add `randomInt()` function

--- web/src/util.ts
+++ web/src/util.ts
@@ -1,3 +1,7 @@
 export function isExternalUrl(url: string) {
   return url.match(/\w+\:*/) ? true : false;
 }
+
+export function randomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
