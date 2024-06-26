e84f5011b411fdc79b96a14609e6bb3d21be6c25
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-26 21:03:56 +0200

    ✨ util.ts: Add `sanityImageUrl()` function

--- web/src/util.ts
+++ web/src/util.ts
@@ -1,7 +1,17 @@
+import { sanityClient } from 'sanity:client';
+import imageUrlBuilder from '@sanity/image-url';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+
 export function isExternalUrl(url: string) {
   return url.match(/\w+\:*/) ? true : false;
 }
 
 export function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const builder = imageUrlBuilder(sanityClient);
+
+export function sanityImageUrl(source: SanityImageSource) {
+  return builder.image(source);
 }
