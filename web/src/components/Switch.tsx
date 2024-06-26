71c67abd0a3239068f8de641c983cd0129d21076
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-26 21:01:18 +0200

    ✨ Web: Add Switch component

---
+++ web/src/components/Switch.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+
+interface Props {
+  /** The state of the switch when it is initially rendered. Use when you do not need to control its state. */
+  defaultChecked?: boolean;
+  /** The controlled state of the switch. */
+  checked?: boolean;
+  size?: 'small' | 'medium';
+  onChange?: (isChecked?: boolean) => void;
+}
+
+export default function Switch({
+  defaultChecked = false,
+  checked,
+  size = 'medium',
+  onChange,
+}: Props) {
+  const [isChecked, setIsChecked] = useState(defaultChecked);
+
+  useEffect(() => {
+    if (checked !== undefined) {
+      setIsChecked(checked);
+    }
+  }, [checked]);
+
+  const handleToggle = () => {
+    // Only update the state if the switch is not controlled
+    if (checked == undefined) {
+      const newValue = !isChecked;
+      setIsChecked(newValue);
+      if (onChange) onChange(newValue);
+    } else {
+      if (onChange) onChange();
+    }
+  };
+
+  return (
+    <div
+      className={`relative inline-block rounded-full transition-colors duration-300 ease-smooth ${size === 'small' ? 'h-8 w-14' : 'h-10 w-[72px]'} ${isChecked ? 'bg-primary' : 'border border-strong bg-default'}`}
+    >
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleToggle}
+        className="absolute inset-0 z-10 cursor-pointer opacity-0"
+      />
+      <span
+        className={`absolute bottom-1 left-1 top-1 aspect-square rounded-full transition-all duration-300 ease-smooth ${isChecked ? 'translate-x-full bg-default' : 'bg-gray-400'}`}
+      ></span>
+    </div>
+  );
+}
