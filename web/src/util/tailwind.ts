109d8b6219102353754b3cc570d7bab8084a6f83
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-28 21:38:51 +0200

    ✨ Web: Add util/tailwind.ts

---
+++ web/src/util/tailwind.ts
@@ -0,0 +1,6 @@
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfig from '@/../tailwind.config.mjs';
+
+export function useTailwindConfig() {
+  return resolveConfig(tailwindConfig);
+}
