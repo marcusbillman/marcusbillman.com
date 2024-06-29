b8a81e64efe16fcb259c13929f401adeb7c16b7e
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-06-29 21:18:50 +0200

    ✨ Contact page: Add FullContactSection component and implement contact form

---
+++ web/src/components/sections/FullContactSection.tsx
@@ -0,0 +1,105 @@
+import {
+  PaperPlaneTilt,
+  EnvelopeSimple,
+  Copy,
+} from '@phosphor-icons/react/dist/ssr';
+import CopyToClipboard from '@/components/CopyToClipboard';
+import Button from '@/components/Button';
+import DotGrid from '@/components/DotGrid';
+import ContactFormModal from '@/components/ContactFormModal';
+import { LinkedIn } from '@/components/icons';
+import { useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
+
+export default function FullContactSection() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const EMAIL = 'hello@marcusbillman.com';
+
+  return (
+    <section className="flex min-h-[75vh] flex-col items-center justify-center px-4 py-32 lg:p-32">
+      <h1 className="text-balance text-center text-5xl lg:text-8xl">
+        Let’s get in touch! 💬
+      </h1>
+      <Button
+        text="Open contact form"
+        icon={PaperPlaneTilt}
+        style="primary"
+        className="mt-16"
+        onClick={() => setIsModalOpen(true)}
+      />
+      <AnimatePresence>
+        {isModalOpen && (
+          <ContactFormModal onClose={() => setIsModalOpen(false)} />
+        )}
+      </AnimatePresence>
+      <span className="mt-6 text-xl text-subtle">or</span>
+      <div className="mt-6 flex w-full max-w-5xl flex-col gap-4 md:flex-row lg:gap-8">
+        <div className="flex flex-col gap-4 rounded-2xl border border-default bg-default p-4 md:flex-1 lg:p-6">
+          <div className="flex items-center gap-2">
+            <EnvelopeSimple size={24} className="lg:hidden" />
+            <EnvelopeSimple size={32} className="hidden lg:block" />
+            <span className="text-xl font-medium lg:text-2xl">{EMAIL}</span>
+          </div>
+          <div className="flex gap-2 lg:gap-3">
+            <CopyToClipboard text={EMAIL}>
+              <Button
+                text="Copy"
+                icon={Copy}
+                size="small"
+                className="lg:hidden"
+              />
+              <Button
+                text="Copy"
+                icon={Copy}
+                size="medium"
+                className="hidden lg:flex"
+              />
+            </CopyToClipboard>
+            <Button
+              text="Open"
+              href={`mailto:${EMAIL}`}
+              style="subtle"
+              size="small"
+              className="lg:hidden"
+            />
+            <Button
+              text="Open"
+              href={`mailto:${EMAIL}`}
+              style="subtle"
+              size="medium"
+              className="hidden lg:flex"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 rounded-2xl border border-default bg-default p-4 md:flex-1 lg:p-6">
+          <div className="flex items-center gap-2">
+            <LinkedIn size={24} className="lg:hidden" />
+            <LinkedIn size={32} className="hidden lg:block" />
+            <span className="text-xl font-medium lg:text-2xl">LinkedIn</span>
+          </div>
+          <div className="flex gap-2 lg:gap-3">
+            <Button
+              text="Visit profile"
+              href={`https://m-b.me/linkedin`}
+              style="subtle"
+              size="small"
+              className="lg:hidden"
+            />
+            <Button
+              text="Visit profile"
+              href={`https://m-b.me/linkedin`}
+              style="subtle"
+              size="medium"
+              className="hidden lg:flex"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Backgrounds */}
+      <DotGrid dim="default" />
+      <div className='absolute bottom-[10%] left-[10%] -z-10 aspect-square w-full translate-x-1/3 bg-[url("/assets/images/glow-orange.png")] bg-cover bg-center'></div>
+      <div className='absolute right-[10%] top-[10%] -z-10 aspect-square w-full -translate-x-1/3 bg-[url("/assets/images/glow-blueberry.png")] bg-cover bg-center'></div>
+    </section>
+  );
+}
