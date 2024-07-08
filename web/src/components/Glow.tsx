8ae5157c9929b590d5d27fbf2c0473b3b61cc2a6
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-08 15:23:31 +0200

    🐛 Glow: Fix background image not appearing

--- web/src/components/Glow.tsx
+++ web/src/components/Glow.tsx
@@ -7,9 +7,18 @@ interface Props {
  * Decorative glow effect. Absolutely positioned and square. Remember to set width or height using styles.
  */
 export default function Glow({ color, className }: Props) {
+  function bgClass() {
+    if (color === 'blueberry') {
+      return 'bg-[url("/assets/images/glow-blueberry.png")]';
+    }
+    if (color === 'orange') {
+      return 'bg-[url("/assets/images/glow-orange.png")]';
+    }
+  }
+
   return (
     <div
-      className={`${className} absolute -z-10 aspect-square bg-[url("/assets/images/glow-${color}.png")] bg-cover bg-center`}
+      className={`${className} absolute -z-10 aspect-square ${bgClass()} bg-cover bg-center`}
     />
   );
 }
