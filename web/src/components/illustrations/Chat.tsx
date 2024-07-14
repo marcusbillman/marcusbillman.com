f8e42cddc47768ad8abf65e75650f5972a134d83
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-14 19:21:37 +0200

    ♻️ Chat illustration: Replace `stroke-width`with `strokeWidth`

--- web/src/components/illustrations/Chat.tsx
+++ web/src/components/illustrations/Chat.tsx
@@ -16,7 +16,7 @@ export default function Chat({
       <path
         d="M178.5 12V49.5H94.5H91C85.201 49.5 80.5 54.201 80.5 60V74H40C37.079 74 34.2368 74.9474 31.9 76.7L19.5 86V75.5V74H18H12C6.20101 74 1.5 69.299 1.5 63.5V12C1.5 6.20101 6.20101 1.5 12 1.5H168C173.799 1.5 178.5 6.20101 178.5 12Z"
         stroke="#3551B6"
-        stroke-width="3"
+        strokeWidth="3"
       />
       <rect x="14" y="14" width="30" height="3" fill="#3551B6" />
       <rect x="93" y="14" width="30" height="3" fill="#3551B6" />
@@ -29,7 +29,7 @@ export default function Chat({
       <path
         d="M205.186 122H203.686V123.5V133.981L191.492 124.74C189.145 122.962 186.282 122 183.338 122H91C85.201 122 80.5 117.299 80.5 111.5V60C80.5 54.201 85.201 49.5 91 49.5H211C216.799 49.5 221.5 54.201 221.5 60V111.5C221.5 117.299 216.799 122 211 122H205.186Z"
         stroke="#4265E3"
-        stroke-width="3"
+        strokeWidth="3"
       />
       <rect x="93" y="62" width="30" height="3" fill="#4265E3" />
       <rect x="127" y="62" width="15" height="3" fill="#4265E3" />
@@ -39,19 +39,19 @@ export default function Chat({
       <rect x="93" y="84" width="30" height="3" fill="#4265E3" />
       <rect x="150" y="73" width="40" height="3" fill="#4265E3" />
       <rect x="93" y="73" width="19" height="3" fill="#4265E3" />
-      <circle cx="16" cy="109" r="14.5" stroke="#3551B6" stroke-width="3" />
-      <circle cx="16" cy="107" r="5.5" stroke="#3551B6" stroke-width="3" />
+      <circle cx="16" cy="109" r="14.5" stroke="#3551B6" strokeWidth="3" />
+      <circle cx="16" cy="107" r="5.5" stroke="#3551B6" strokeWidth="3" />
       <path
         d="M16 123.5C12.0297 123.5 8.43783 121.982 5.80413 119.526C7.68048 115.966 11.5277 113.5 16 113.5C20.4723 113.5 24.3195 115.966 26.1959 119.526C23.5622 121.982 19.9703 123.5 16 123.5Z"
         stroke="#3551B6"
-        stroke-width="3"
+        strokeWidth="3"
       />
-      <circle cx="207" cy="157" r="14.5" stroke="#4265E3" stroke-width="3" />
-      <circle cx="207" cy="155" r="5.5" stroke="#4265E3" stroke-width="3" />
+      <circle cx="207" cy="157" r="14.5" stroke="#4265E3" strokeWidth="3" />
+      <circle cx="207" cy="155" r="5.5" stroke="#4265E3" strokeWidth="3" />
       <path
         d="M207 171.5C203.03 171.5 199.438 169.982 196.804 167.526C198.68 163.966 202.528 161.5 207 161.5C211.472 161.5 215.32 163.966 217.196 167.526C214.562 169.982 210.97 171.5 207 171.5Z"
         stroke="#4265E3"
-        stroke-width="3"
+        strokeWidth="3"
       />
     </svg>
   ) : (
@@ -66,7 +66,7 @@ export default function Chat({
       <path
         d="M178.5 12V49.5H94.5H91C85.201 49.5 80.5 54.201 80.5 60V74H40C37.079 74 34.2368 74.9474 31.9 76.7L19.5 86V75.5V74H18H12C6.20101 74 1.5 69.299 1.5 63.5V12C1.5 6.20101 6.20101 1.5 12 1.5H168C173.799 1.5 178.5 6.20101 178.5 12Z"
         stroke="#3551B6"
-        stroke-width="3"
+        strokeWidth="3"
       />
       <rect x="14" y="14" width="30" height="3" fill="#3551B6" />
       <rect x="93" y="14" width="30" height="3" fill="#3551B6" />
@@ -80,7 +80,7 @@ export default function Chat({
         d="M205.186 122H203.686V123.5V133.981L191.492 124.74C189.145 122.962 186.282 122 183.338 122H91C85.201 122 80.5 117.299 80.5 111.5V60C80.5 54.201 85.201 49.5 91 49.5H211C216.799 49.5 221.5 54.201 221.5 60V111.5C221.5 117.299 216.799 122 211 122H205.186Z"
         fill="white"
         stroke="#4265E3"
-        stroke-width="3"
+        strokeWidth="3"
       />
       <rect x="93" y="62" width="30" height="3" fill="#4265E3" />
       <rect x="127" y="62" width="15" height="3" fill="#4265E3" />
@@ -90,12 +90,12 @@ export default function Chat({
       <rect x="93" y="84" width="30" height="3" fill="#4265E3" />
       <rect x="150" y="73" width="40" height="3" fill="#4265E3" />
       <rect x="93" y="73" width="19" height="3" fill="#4265E3" />
-      <circle cx="16" cy="109" r="14.5" stroke="#3551B6" stroke-width="3" />
-      <circle cx="16" cy="107" r="5.5" stroke="#3551B6" stroke-width="3" />
+      <circle cx="16" cy="109" r="14.5" stroke="#3551B6" strokeWidth="3" />
+      <circle cx="16" cy="107" r="5.5" stroke="#3551B6" strokeWidth="3" />
       <path
         d="M16 123.5C12.0297 123.5 8.43783 121.982 5.80413 119.526C7.68048 115.966 11.5277 113.5 16 113.5C20.4723 113.5 24.3195 115.966 26.1959 119.526C23.5622 121.982 19.9703 123.5 16 123.5Z"
         stroke="#3551B6"
-        stroke-width="3"
+        strokeWidth="3"
       />
       <circle
         cx="207"
@@ -103,13 +103,13 @@ export default function Chat({
         r="14.5"
         fill="white"
         stroke="#4265E3"
-        stroke-width="3"
+        strokeWidth="3"
       />
-      <circle cx="207" cy="155" r="5.5" stroke="#4265E3" stroke-width="3" />
+      <circle cx="207" cy="155" r="5.5" stroke="#4265E3" strokeWidth="3" />
       <path
         d="M207 171.5C203.03 171.5 199.438 169.982 196.804 167.526C198.68 163.966 202.528 161.5 207 161.5C211.472 161.5 215.32 163.966 217.196 167.526C214.562 169.982 210.97 171.5 207 171.5Z"
         stroke="#4265E3"
-        stroke-width="3"
+        strokeWidth="3"
       />
     </svg>
   );
