78422d6a9ad138d44bc6089ed807fe73dd576cdf
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-16 23:39:44 +0200

    ➕ Web: Add and configure `@astrojs/vercel` for static deployment

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sanity from '@sanity/astro';
+import vercel from '@astrojs/vercel/static';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
@@ -11,8 +11,9 @@ export default defineConfig({
     sanity({
       projectId: '2l3afo6f',
       dataset: 'production',
-      // Set useCdn to false if you're building statically.
       useCdn: false,
     }),
   ],
+  output: 'static',
+  adapter: vercel(),
 });
