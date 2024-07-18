3a9c468d774f257a2a8bd2103830f2c619bddd3c
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-18 17:19:43 +0200

    ♿️ MoreThingsSection: Use grid to avoid wrapper divs

--- web/src/components/sections/MoreThingsSection.tsx
+++ web/src/components/sections/MoreThingsSection.tsx
@@ -17,71 +17,65 @@ export default function MoreThingsSection() {
       <h2 className="text-4xl font-medium lg:text-6xl">
         More things made by me 🍱
       </h2>
-      <ul className="mt-12 grid gap-6 lg:mt-16 lg:gap-8">
-        <div className="flex min-h-96 flex-col gap-6 lg:h-[20vw] lg:flex-row lg:gap-8">
-          <BentoCard
-            title="Side projects"
-            description="Web apps and experiments"
-            icon={Flask}
-            href="portfolio"
-            className="h-[50vh] flex-grow bg-gradient-to-b from-[#bfcaf1] to-[#889ef1] text-black lg:h-auto xl:flex-[2]"
-          >
-            <img
-              src="/assets/images/screenshot-soundboard.jpg"
-              alt="App screenshot"
-              className="absolute left-[20%] top-[0%] h-48 rotate-[30deg] rounded-2xl shadow-xl"
-              aria-hidden
-            />
-            <img
-              src="/assets/images/screenshot-cooldownload.jpg"
-              alt="App screenshot"
-              className="absolute bottom-[0%] left-[35%] h-48 rotate-[30deg] rounded-2xl shadow-xl"
-              aria-hidden
-            />
-            <ButtonClick className="absolute left-[90%] top-[0%] rotate-[30deg] drop-shadow-xl" />
-            <Phone className="absolute left-[80%] top-[15%] rotate-[30deg] drop-shadow-xl" />
-            <CodeBlock className="absolute left-[90%] top-[60%] rotate-[30deg] drop-shadow-xl" />
+      <ul className="mt-12 grid gap-6 md:grid-cols-6 md:grid-rows-[repeat(4,max(10vw,12rem))] lg:mt-16 lg:gap-8">
+        <BentoCard
+          title="Side projects"
+          description="Web apps and experiments"
+          icon={Flask}
+          href="portfolio"
+          className="h-[50vh] bg-gradient-to-b from-[#bfcaf1] to-[#889ef1] text-black md:col-span-3 md:row-span-2 md:h-auto xl:col-span-4"
+        >
+          <img
+            src="/assets/images/screenshot-soundboard.jpg"
+            alt="App screenshot"
+            className="absolute left-[20%] top-[0%] h-48 rotate-[30deg] rounded-2xl shadow-xl"
+            aria-hidden
+          />
+          <img
+            src="/assets/images/screenshot-cooldownload.jpg"
+            alt="App screenshot"
+            className="absolute bottom-[0%] left-[35%] h-48 rotate-[30deg] rounded-2xl shadow-xl"
+            aria-hidden
+          />
+          <ButtonClick className="absolute left-[90%] top-[0%] rotate-[30deg] drop-shadow-xl" />
+          <Phone className="absolute left-[80%] top-[15%] rotate-[30deg] drop-shadow-xl" />
+          <CodeBlock className="absolute left-[90%] top-[60%] rotate-[30deg] drop-shadow-xl" />
+          <DotGrid />
+        </BentoCard>
+        <BentoCard
+          title="Dribbble"
+          description="Design explorations"
+          icon={Dribbble}
+          href="https://m-b.me/dribbble"
+          className="h-[50vh] bg-[url('/assets/images/designer-desk.jpg')] bg-cover bg-center md:col-span-3 md:row-span-2 md:h-auto xl:col-span-2"
+        >
+          <DotGrid dim="strong" />
+        </BentoCard>
+        <BentoCard
+          title="SoundCloud"
+          description="Music"
+          icon={SoundCloud}
+          href="https://m-b.me/soundcloud"
+          className="h-[50vh] bg-[url('/assets/images/music-flatlay.jpg')] bg-cover bg-center text-white md:col-span-3 md:row-span-2 md:h-auto"
+        >
+          <div className="absolute inset-0 isolate bg-gradient-to-b from-[#FF5722CC] to-[#FF5722]">
             <DotGrid />
-          </BentoCard>
-          <BentoCard
-            title="Dribbble"
-            description="Design explorations"
-            icon={Dribbble}
-            href="https://m-b.me/dribbble"
-            className="h-[50vh] flex-grow bg-[url('/assets/images/designer-desk.jpg')] bg-cover bg-center lg:h-auto xl:flex-[1]"
-          >
-            <DotGrid dim="strong" />
-          </BentoCard>
-        </div>
-        <div className="flex min-h-96 flex-col gap-6 lg:h-[20vw] lg:flex-row lg:gap-8">
-          <BentoCard
-            title="SoundCloud"
-            description="Music"
-            icon={SoundCloud}
-            href="https://m-b.me/soundcloud"
-            className="h-[50vh] flex-grow bg-[url('/assets/images/music-flatlay.jpg')] bg-cover bg-center text-white lg:h-auto lg:flex-1"
-          >
-            <div className="absolute inset-0 isolate bg-gradient-to-b from-[#FF5722CC] to-[#FF5722]">
-              <DotGrid />
-            </div>
-          </BentoCard>
-          <div className="flex flex-col gap-6 lg:flex-1 lg:gap-8">
-            <BentoCard
-              title="GitHub"
-              description="Code"
-              icon={GitHub}
-              href="https://m-b.me/github"
-              className="flex-grow border border-transparent hover:border-default hover:bg-default lg:h-auto"
-            />
-            <BentoCard
-              title="LinkedIn"
-              description="Networking"
-              icon={LinkedIn}
-              href="https://m-b.me/linkedin"
-              className="flex-grow border border-transparent hover:border hover:bg-default lg:h-auto"
-            />
           </div>
-        </div>
+        </BentoCard>
+        <BentoCard
+          title="GitHub"
+          description="Code"
+          icon={GitHub}
+          href="https://m-b.me/github"
+          className="border border-transparent hover:border-default hover:bg-default md:col-span-3 md:h-auto"
+        />
+        <BentoCard
+          title="LinkedIn"
+          description="Networking"
+          icon={LinkedIn}
+          href="https://m-b.me/linkedin"
+          className="border border-transparent hover:border hover:bg-default md:col-span-3 md:h-auto"
+        />
       </ul>
     </section>
   );
