cae139289cfb07fb105078196721bf0e2ebc76bb
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-19 16:54:49 +0200

    🔧 Configure Prettier and ESLint at root

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import astroPlugin from 'eslint-plugin-astro';
+import reactPlugin from 'eslint-plugin-react';
+import tsEslint from 'typescript-eslint';
+
+export default [
+  { ignores: ['studio/.sanity/'] },
+  {
+    plugins: {
+      reactPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...astroPlugin.configs.recommended,
+  prettierConfig,
+];
---
+++ prettier.config.mjs
@@ -0,0 +1,31 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
+  tailwindConfig: './web/tailwind.config.mjs',
+  importOrder: [
+    '<TYPES>', // Third-party types
+    '<TYPES>^[.]', // Local types
+    '',
+    '^react$', // React imports
+    '<THIRD_PARTY_MODULES>', // Third-party modules
+    '',
+    '^(@|@studio)(/.*)$', // Aliased imports
+    '^[.]', // Local imports
+  ],
+};
