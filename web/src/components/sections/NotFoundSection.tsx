9adcf940b9332f46a15c1f7608c246bb17c20f97
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-20 20:53:11 +0200

    ✨ 404 page: Implement full interactivity

--- web/src/components/sections/NotFoundSection.tsx
+++ web/src/components/sections/NotFoundSection.tsx
@@ -1,44 +1,91 @@
 import { House } from '@phosphor-icons/react/dist/ssr';
 import { getImage } from 'astro:assets';
+import { motion } from 'framer-motion';
 
 import bg from '@/assets/images/hero-bg-still.jpg';
 import Button from '@/components/Button';
 import DotGrid from '@/components/DotGrid';
+import Sandbox from '@/components/Sandbox';
 
 const optimizedBg = await getImage({ src: bg });
 
-export default function FullContactSection() {
+export default function NotFoundSection() {
   return (
-    <section className="isolate flex min-h-screen flex-col items-center justify-center bg-primary lg:p-16 2xl:p-32">
+    <section className="relative isolate flex min-h-screen flex-col bg-primary lg:block">
       {/* Decoration */}
-      <div className="relative w-full flex-grow">
-        <span
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-cover bg-clip-text bg-center text-[40vh] font-bold tracking-tighter text-transparent"
-          style={{ backgroundImage: `url(${optimizedBg.src})` }}
+      <Sandbox
+        randomize={false}
+        className="relative z-10 flex-grow text-4xl transition-opacity md:text-6xl lg:absolute lg:inset-4 lg:text-8xl"
+      >
+        <motion.span
+          className="left-[-10%] top-[10%] bg-cover bg-clip-text bg-left-top text-[40vh] font-bold leading-[0.8] tracking-tighter text-transparent lg:left-[20%]"
+          style={{ backgroundImage: `url(${optimizedBg.src})`, rotate: 15 }}
         >
-          <span>4</span>
-          <span>0</span>
-          <span>4</span>
-        </span>
-      </div>
+          4
+        </motion.span>
+        <motion.span
+          className="left-[25%] top-[45%] bg-cover bg-clip-text bg-bottom text-[40vh] font-bold leading-[0.8] tracking-tighter text-transparent lg:left-[35%]"
+          style={{ backgroundImage: `url(${optimizedBg.src})`, rotate: -8 }}
+        >
+          0
+        </motion.span>
+        <motion.span
+          className="left-[70%] top-[20%] bg-cover bg-clip-text bg-right-top text-[40vh] font-bold leading-[0.8] tracking-tighter text-transparent"
+          style={{ backgroundImage: `url(${optimizedBg.src})`, rotate: -20 }}
+        >
+          4
+        </motion.span>
+        <motion.span
+          className="left-[15%] top-[30%] drop-shadow-lg lg:top-[15%]"
+          aria-hidden
+        >
+          🧐
+        </motion.span>
+        <motion.span
+          className="left-[70%] top-[20%] drop-shadow-lg lg:left-[60%] lg:top-[10%]"
+          aria-hidden
+        >
+          🚧
+        </motion.span>
+        <motion.span
+          className="left-[85%] top-[75%] drop-shadow-lg lg:top-[20%]"
+          aria-hidden
+        >
+          😵
+        </motion.span>
+        <motion.span
+          className="left-[55%] top-[60%] drop-shadow-lg lg:top-[40%]"
+          aria-hidden
+        >
+          🗺
+        </motion.span>
+        <motion.span
+          className="left-[10%] top-[90%] drop-shadow-lg lg:top-[50%]"
+          aria-hidden
+        >
+          🚷
+        </motion.span>
+      </Sandbox>
 
       {/* Content */}
-      <div className="z-10 flex flex-col gap-6 rounded-2xl p-6 text-on-primary lg:bg-default lg:text-default xl:flex-row xl:items-end xl:gap-16">
-        <h1 className="text-balance text-4xl lg:text-6xl">
-          Woah, you found the 404 page!
-        </h1>
-        <div className="flex flex-col gap-6 md:flex-row md:items-center xl:gap-16">
-          <p className="text-xl md:text-balance">
-            Though, you were probably looking for something else. Check the URL
-            or start from the home page.
-          </p>
-          <Button
-            text="Let's go home"
-            icon={House}
-            iconSide="left"
-            href="/"
-            className="flex-shrink-0"
-          />
+      <div className="h-fit lg:absolute lg:bottom-16 lg:left-16 lg:right-16 2xl:bottom-32 2xl:left-32 2xl:right-32">
+        <div className="mx-auto flex flex-col gap-6 rounded-2xl p-6 text-on-primary lg:container lg:bg-default lg:text-default xl:flex-row xl:items-end xl:gap-16">
+          <h1 className="text-balance text-4xl lg:text-6xl">
+            Woah, you found the 404 page!
+          </h1>
+          <div className="flex flex-col gap-6 md:flex-row md:items-center xl:gap-16">
+            <p className="text-xl md:text-balance">
+              Though, you were probably looking for something else. Check the
+              URL or start from the home page.
+            </p>
+            <Button
+              text="Let's go home"
+              icon={House}
+              iconSide="left"
+              href="/"
+              className="flex-shrink-0"
+            />
+          </div>
         </div>
       </div>
 
