e3c68e062a3a8a44218fc88c287d5ae185f55625
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-07-28 13:30:50 +0200

    🗃️ imageWithAlt: Add field descriptions, make alt required

--- studio/schemaTypes/imageWithAlt.ts
+++ studio/schemaTypes/imageWithAlt.ts
@@ -1,3 +1,5 @@
+import { Rule } from 'sanity';
+
 export default {
   name: 'imageWithAlt',
   title: 'Image',
@@ -10,11 +12,14 @@ export default {
       name: 'alt',
       type: 'string',
       title: 'Alt text',
+      description: 'Descriptive text that works in place of the image',
+      validation: (Rule: Rule) => Rule.required(),
     },
     {
       name: 'caption',
       type: 'string',
       title: 'Caption',
+      description: 'Supportive text that provides additional context',
     },
   ],
 };
